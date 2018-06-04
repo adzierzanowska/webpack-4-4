45da197e0324879587959a2fb1fc3e73d4b5fbc6
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-06-04 13:40:44 +0200

    Init

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Title = props => 
+<div> 
+    <h1>To do App</h1> 
+    <h3>Tasks to do {props.number}</h3>
+</div>
+
+export default Title;
